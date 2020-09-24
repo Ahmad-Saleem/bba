de77bb923b1928297bb4aa092359eb4763e1ed6f
Ahmad Saleem <it_saleem@hotmail.com>
2020-09-24 19:30:58 +0100

    x

---
+++ build/precache-manifest.e775ba114a7973a16742d3d609177a39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782fafd8d396484618ef826afceca436",
+    "url": "/bba/index.html"
+  },
+  {
+    "revision": "dbcfc4671aae956c7c9c",
+    "url": "/bba/static/css/2.4a1c775f.chunk.css"
+  },
+  {
+    "revision": "13835d3d07cfc07934ce",
+    "url": "/bba/static/css/main.79d63c47.chunk.css"
+  },
+  {
+    "revision": "dbcfc4671aae956c7c9c",
+    "url": "/bba/static/js/2.e1363a4d.chunk.js"
+  },
+  {
+    "revision": "f4327777c32dcac18d2ab1fbd84312a9",
+    "url": "/bba/static/js/2.e1363a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13835d3d07cfc07934ce",
+    "url": "/bba/static/js/main.e0543985.chunk.js"
+  },
+  {
+    "revision": "2cc62619dbcf9b0e883d",
+    "url": "/bba/static/js/runtime-main.1ae28454.js"
+  }
+]);
