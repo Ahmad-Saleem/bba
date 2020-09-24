46a7e7b0ca39120d87dd87bcb93e4aba0803cce1
Ahmad Saleem <it_saleem@hotmail.com>
2020-09-24 19:16:29 +0100

    pro

---
+++ precache-manifest.9dd2f5247239967ab747762df22d3858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c3962571eb890462e810ece97e21a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcfc4671aae956c7c9c",
+    "url": "/static/css/2.4a1c775f.chunk.css"
+  },
+  {
+    "revision": "d8e86fee98f5ac380966",
+    "url": "/static/css/main.79d63c47.chunk.css"
+  },
+  {
+    "revision": "dbcfc4671aae956c7c9c",
+    "url": "/static/js/2.e1363a4d.chunk.js"
+  },
+  {
+    "revision": "f4327777c32dcac18d2ab1fbd84312a9",
+    "url": "/static/js/2.e1363a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e86fee98f5ac380966",
+    "url": "/static/js/main.85cea8a7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
